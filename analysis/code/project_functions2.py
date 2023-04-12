87b03be9b98d0918fc68b7f45de64dab8359486b
ehayton <elliott.hayton@gmail.com>
2023-04-12 09:50:05 -0700

    Some project function work

--- analysis/code/project_functions2.py
+++ analysis/code/project_functions2.py
@@ -5,17 +5,16 @@ import seaborn as sns
 
 
 def load_and_process(url):
-    df_new = (pd.read_csv("../data/raw/FT1000.csv")
-      .drop(columns=['Ranked2021', 'Ranked2020'])
-      .dropna()
-      .query("Employees2017.between(10, 250) and Employees2020.between(10, 250)")
-      .assign(**{'Change % in Employees': lambda x: ((x['Employees2020'] - x['Employees2017']) / x['Employees2020']) * 100}))
+    df = pd.read_csv("../data/raw/FT1000.csv")
+    df_new = (df.drop(columns=['Ranked2021', 'Ranked2020'])
+              .dropna()
+              .query("Employees2017.between(10, 250) and Employees2020.between(10, 250)")
+              .assign(**{'Change % in Employees': lambda x: ((x['Employees2020'] - x['Employees2017']) / x['Employees2020']) * 100}))
 
-    sector_df = (df.groupby("Sector").mean()
+    sector_df = (df_new.groupby("Sector").mean(numeric_only=True)
                  .sort_values(by='Revenue2020', ascending=False)
                  .round(2)
                  .drop(columns=['Rank']))
 
-    return df_new
-
+    return df_new, sector_df
 
